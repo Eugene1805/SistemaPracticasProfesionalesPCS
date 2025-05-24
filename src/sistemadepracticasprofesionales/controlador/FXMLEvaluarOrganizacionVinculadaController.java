9d492cc06e0ca7954dfbe2a71779c9a413df5c57
Eugenio <eugeniosalvadorgonzalez@gmail.com>
2025-05-24 14:27:31 -0600

    Add CU_Evaluar_Organizacion_Vinculada, FXML and controller for CU_Asignar_Proyectos

--- src/sistemadepracticasprofesionales/controlador/FXMLEvaluarOrganizacionVinculadaController.java
+++ src/sistemadepracticasprofesionales/controlador/FXMLEvaluarOrganizacionVinculadaController.java
@@ -1,12 +1,26 @@
 package sistemadepracticasprofesionales.controlador;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.stream.Stream;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
+import sistemadepracticasprofesionales.modelo.dao.EvaluacionOrganizacionDAO;
+import sistemadepracticasprofesionales.modelo.pojo.EvaluacionOrganizacion;
+import sistemadepracticasprofesionales.modelo.pojo.ResultadoOperacion;
+import sistemadepracticasprofesionales.utilidades.Utilidad;
+import sistemadepracticasprofesionales.utilidades.validacion.ValidadorFormulario;
+import sistemadepracticasprofesionales.utilidades.validacion.estrategias.ComboValidationStrategy;
 
 /**
  * FXML Controller class
@@ -33,20 +47,92 @@ public class FXMLEvaluarOrganizacionVinculadaController implements Initializable
     @FXML
     private ComboBox<Integer> cbAccesoRecursos;
 
+    private ValidadorFormulario validarFormulario;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        //FIX obtener el nombre de la Organizacion Vinculada del Usuario
+        lbNombreOV.setText("HighTek");
+        cargarComboBoxes();
+        inicializarValidaciones();
     }    
 
     @FXML
     private void btnClicAceptar(ActionEvent event) {
+        if(validarFormulario.validate()){
+            EvaluacionOrganizacion evaluacionOrganizacion = obtenerEvaluacion();
+            guardarEvaluacionOrganizacion(evaluacionOrganizacion);
+        }else{
+            Utilidad.mostrarAlertaSimple(Alert.AlertType.WARNING,"Datos invalidos",
+                    "Hay campos con datos invalidos");
+        }
     }
 
     @FXML
     private void btnClicCancelar(ActionEvent event) {
+        Alert alerta = Utilidad.mostrarAlertaConfirmacion("Confirmacion Cancelar",
+                "¿Estás seguro de que deseas cancelar?");
+        Optional<ButtonType> resultado = alerta.showAndWait();
+        if(resultado.get() == ButtonType.APPLY){
+            regresarAlDashboard();
+        }
     }
     
+    private void cargarComboBoxes(){
+        ObservableList<Integer> grados = FXCollections.observableArrayList();
+        grados.addAll(1,2,3,4,5);
+        cbAmbiente_laboral.setItems(grados);
+        cbOportunidadesAprendizaje.setItems(grados);
+        cbClaridadActividades.setItems(grados);
+        cbNivelRelacionActividades.setItems(grados);
+        cbAccesibilidad.setItems(grados);
+        cbAccesoRecursos.setItems(grados);
+    }
+    
+    private EvaluacionOrganizacion obtenerEvaluacion(){
+        EvaluacionOrganizacion evaluacionOrganizacion = new EvaluacionOrganizacion();
+        return evaluacionOrganizacion;
+    }
+    
+    private void guardarEvaluacionOrganizacion(EvaluacionOrganizacion evaluacionOrganizacion){
+        try {
+            ResultadoOperacion resultadoOperacion = EvaluacionOrganizacionDAO.
+                    registrarEvaluacionOrganizacion(evaluacionOrganizacion);
+            if(!resultadoOperacion.isError()){
+                Utilidad.mostrarAlertaSimple(Alert.AlertType.INFORMATION, "Operacion exitosa", 
+                        "Evaluacion de la Organizacion Vinculada registrada con exito");
+                regresarAlDashboard();
+            }else{
+                Utilidad.mostrarAlertaSimple(Alert.AlertType.WARNING, "No se pudo registrar", 
+                        "No fue posible guardar el registro de la evaluacion a la organizacion vinculada");
+            }
+        } catch (SQLException ex) {
+            Utilidad.mostrarAlertaSimple(Alert.AlertType.WARNING, "No hay conexion",
+                    "Lo sentimos no fue posible conectarnos a la base de datos");
+        }
+        
+    }
+    
+    private void inicializarValidaciones(){
+        validarFormulario = new ValidadorFormulario();
+        validarFormulario.addValidation(cbAccesibilidad, new ComboValidationStrategy(true));
+        validarFormulario.addValidation(cbAccesoRecursos, new ComboValidationStrategy(true));
+        validarFormulario.addValidation(cbAmbiente_laboral, new ComboValidationStrategy(true));
+        validarFormulario.addValidation(cbClaridadActividades, new ComboValidationStrategy(true));
+        validarFormulario.addValidation(cbNivelRelacionActividades, new ComboValidationStrategy(true));
+        validarFormulario.addValidation(cbOportunidadesAprendizaje, new ComboValidationStrategy(true));
+        validarFormulario.addCleanupAction(()->{
+            Stream.of(cbAccesibilidad,cbAccesoRecursos,cbAmbiente_laboral,cbClaridadActividades,
+                        cbNivelRelacionActividades,cbOportunidadesAprendizaje)
+          .filter(tf -> !tf.getStyle().isEmpty())
+          .findFirst()
+          .ifPresent(Control::requestFocus);//Le da el foco al primer campo con texto vacio
+        });
+    }
+    
+    private void regresarAlDashboard(){
+        Utilidad.obtenerEscenario(lbNombreOV).close();
+    }
 }
---
+++ src/sistemadepracticasprofesionales/modelo/dao/EvaluacionOrganizacionDAO.java
@@ -0,0 +1,43 @@
+package sistemadepracticasprofesionales.modelo.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import sistemadepracticasprofesionales.modelo.ConexionBD;
+import sistemadepracticasprofesionales.modelo.pojo.EvaluacionOrganizacion;
+import sistemadepracticasprofesionales.modelo.pojo.ResultadoOperacion;
+
+/**
+ *
+ * @author eugen
+ * Fecha: 24/05/25
+ * Descripcion: DAO para guardar le evaluacion del estudiante a la Organizacion vinculada
+ */
+public class EvaluacionOrganizacionDAO {
+    public static ResultadoOperacion registrarEvaluacionOrganizacion(
+            EvaluacionOrganizacion evaluacionOrganizacion) throws SQLException{
+        ResultadoOperacion resultadoOperacion = new ResultadoOperacion();
+        Connection conexionDB = ConexionBD.abrirConexion();
+        if(conexionDB != null){
+            String consulta = "INSERT INTO evaluacion_organizacion (claridad_actividades, "
+                    + "nivel_relacion_actividades, accesibilidad, ambiente_laboral, oportunidades_aprendizaje "
+                    + "acceso_recursos) VALUES (?,?,?,?,?,?)";
+            PreparedStatement sentencia = conexionDB.prepareCall(consulta);
+            sentencia.setInt(1, evaluacionOrganizacion.getClaridadActividades());
+            sentencia.setInt(2, evaluacionOrganizacion.getNivelRelacionActividades());
+            sentencia.setInt(3, evaluacionOrganizacion.getAccesibilidad());
+            sentencia.setInt(4, evaluacionOrganizacion.getAmbienteLaboral());
+            sentencia.setInt(5, evaluacionOrganizacion.getOportunidades_aprendizaje());
+            sentencia.setInt(6, evaluacionOrganizacion.getAccesoRecursos());
+            
+            int filasAfectadas = sentencia.executeUpdate();
+            resultadoOperacion.setError(filasAfectadas <= 0);
+            resultadoOperacion.setMensaje(resultadoOperacion.isError() ?
+                    "Lo sentimos no fue posible registar los datos de la Evaluacion a la Organizacion Vinculada" :
+                    "La informacion de la Evaluacion a la Organizacion Vinculada fue registrada correctamente");
+        }else{
+            throw new SQLException("No hay conexion");
+        }
+        return resultadoOperacion;        
+    }
+}
